86f1995e759e75398057ca735e77a5f335ab810d
igwe2012 <everebukz@yahoo.com>
2017-07-13 21:33:13 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace window2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Guess the correct number. you can only try three times";
+            // 
+            // Text
+            // 
+            this.Text.Location = new System.Drawing.Point(83, 99);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(100, 20);
+            this.Text.TabIndex = 2;
+            this.Text.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 286);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Text;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace window2
+{
+    public partial class Form1 : Form
+    {
+        //     int number;
+        // static   int counter = 0;
+        //    int mainNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        int number;
+        int counter = 0;
+        int mainNum;
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            Random random = new Random();
+            int mainNum = random.Next(10);
+
+            if (counter < 3)
+            {
+                try
+                {
+                    number = int.Parse(Text.Text);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Text.Clear();
+                    counter++;
+                    return;
+                }
+                if (number == mainNum)
+                {
+                    MessageBox.Show("Hurray!! you won a car");
+                    var show = MessageBox.Show("Do u want to play again?", "prompt", MessageBoxButtons.YesNo);
+                    if (show == DialogResult.Yes)
+                    {
+
+                        return;
+
+                    }
+                    else
+                    {
+                    Application.Exit();
+                }
+                   // Application.Exit();
+                }
+                else if (number != mainNum)
+                {
+                    MessageBox.Show("incorrect, Try again");
+                    Text.Clear();
+                }
+                counter++;
+            }
+            else
+            {
+                MessageBox.Show("You have exceeded the number of tries");
+               
+                    Application.Exit();
+                }
+                // Application.Exit();
+            }
+        }
+    }
+   
